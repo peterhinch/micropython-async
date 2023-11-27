e815e9bf8638a1e54cc3b58cd810d679f49a5619
Peter Hinch <peter@hinch.me.uk>
2023-11-27 16:44:07 +0000

    auart.py: Add code comment re timeout.

--- v3/as_demos/auart.py
+++ v3/as_demos/auart.py
@@ -3,22 +3,27 @@
 # Copyright Peter Hinch 2017-2022 Released under the MIT license
 # Link X1 and X2 to test.
 
+# We run with no UART timeout: UART read never blocks.
 import uasyncio as asyncio
 from machine import UART
+
 uart = UART(4, 9600, timeout=0)
+
 
 async def sender():
     swriter = asyncio.StreamWriter(uart, {})
     while True:
-        swriter.write('Hello uart\n')
+        swriter.write("Hello uart\n")
         await swriter.drain()
         await asyncio.sleep(2)
+
 
 async def receiver():
     sreader = asyncio.StreamReader(uart)
     while True:
         res = await sreader.readline()
-        print('Received', res)
+        print("Received", res)
+
 
 async def main():
     asyncio.create_task(sender())
@@ -26,13 +31,15 @@ async def main():
     while True:
         await asyncio.sleep(1)
 
+
 def test():
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
     finally:
         asyncio.new_event_loop()
-        print('as_demos.auart.test() to run again.')
+        print("as_demos.auart.test() to run again.")
+
 
 test()
